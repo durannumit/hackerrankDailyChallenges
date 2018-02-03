347ddd0840c80bd5ee0853fa8244efbbd8a52a7d
Ümit Duran <durannumit@gmail.com>
2018-02-04 01:02:36 +0300

    day 15 added

---
+++ day15/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+ public static  Node insert(Node head,int data) {
+        //Complete this method
+       
+         if (head == null) 
+             head = new Node(data);
+        else {
+            Node node = head;
+            while (node.next != null) {
+                 node = node.next;
+            }
+               
+            node.next = new Node(data);
+        }
+        return head;
+    }
+
+public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
